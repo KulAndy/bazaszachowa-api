59f0276f65b0680fe22e76a0f3344fc381f7e6b6
Andrzej Kulesza <andykrk22@gmail.com>
2025-07-10 15:16:04 +0200

    hardeneding API

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express, { Express, Request, Response } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import path from "node:path";
 import fs from "node:fs";
 import cron from "node-cron";
 import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import hpp from "hpp";
+import rateLimit from "express-rate-limit";
 
 import playerRouter from "./route/player";
 import playersRouter from "./route/players";
@@ -13,15 +17,26 @@ import mailRouter from "./route/mail";
 import baseRouter from "./route/base";
 import redirectLegacyUrls from "./route/legacy";
 
-const app: Express = express();
-const port: number = 3000;
+const app = express();
+const port = 3000;
+
+app.use(helmet());
+app.use(hpp());
+app.use(compression());
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 500,
+  message: "Too many requests from this IP, try again later.",
+});
+app.use(limiter);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/uploads", express.static("uploads"));
 app.options("*", cors());
 
+app.use("/uploads", express.static("uploads"));
 app.use(redirectLegacyUrls);
 app.use("/player", playerRouter);
 app.use("/players", playersRouter);
@@ -30,11 +45,10 @@ app.use("/games", gamesRouter);
 app.use("/mail", mailRouter);
 app.use("/base", baseRouter);
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (_req, res) => {
   res.send("API");
 });
 
-// Schedule a task to run every day at midnight
 cron.schedule("0 0 * * *", () => {
   fs.readdir("uploads", (err, files) => {
     if (err) {
@@ -54,6 +68,7 @@ cron.schedule("0 0 * * *", () => {
   });
 });
 
+// ===== Start Server =====
 app.listen(port, () => {
   console.info(`Server is listening on port ${port}`);
 });
